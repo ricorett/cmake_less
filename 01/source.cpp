1b74ec07ac3fa063eb60e18a7b559d2b433f2e72
Lana <115147586+ricorett@users.noreply.github.com>
2024-02-19 12:34:25 +0300

    Finish 01 task

---
+++ 01/source.cpp
@@ -0,0 +1,11 @@
+#include "source.h"
+
+int main(){
+
+    std::string name;
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << "Здравствуйте, " << name << "!";
+
+    return 0;
+}
---
+++ 01/source.h
@@ -0,0 +1,8 @@
+
+#ifndef LESS_9_SOURCE_H
+#define LESS_9_SOURCE_H
+
+#include <iostream>
+
+
+#endif //LESS_9_SOURCE_H
